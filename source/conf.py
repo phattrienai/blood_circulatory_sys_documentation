f1747a5e7f1912251a544c9a468184463ed776ed
truyenhay4you <phattrienai@gmail.com>
2026-01-25 18:22:08 +0700

    abc

--- source/conf.py
+++ source/conf.py
@@ -14,7 +14,9 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.githubpages',
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,7 +26,7 @@ exclude_patterns = []
 
 # html_theme = 'alabaster'
 # html_theme = 'furo'
-html_theme = "sphinx_rtd_theme"
-# html_theme = "sphinx_book_theme"
+# html_theme = "sphinx_rtd_theme"
+html_theme = "sphinx_book_theme"
 
 html_static_path = ['_static']
